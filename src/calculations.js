5b0d247406a1bd696cee9c5b62698f9f14cf2098
vulamlomo <emmanuel.rukevwe123@gmail.com>
2026-08-29 03:07:32 +0100

    i added getTotals and getSummary calculations

--- src/calculations.js
+++ src/calculations.js
@@ -1,1 +1,14 @@
-export function getSummary(transactions) {} // this function will calculate the summary of the transactions
+export const getTotals = (transactions) => {     // this function gets the totals of the transactions
+    return transactions.reduce((totals, transaction) => {
+        return transaction.type === 'income' ? {...totals, income: totals.income + transaction.amount}
+        : {...totals, expense: totals.expense + transaction.amount};
+    }, {income: 0, expense: 0});
+}
+
+
+export const getSummary = (transactions) => {   // this gets the Summary of the transactions
+    const summary = getTotals(transactions);
+    const { income, expense } = summary;
+    const balance = income - expense;
+    return { income, expense, balance };
+}
